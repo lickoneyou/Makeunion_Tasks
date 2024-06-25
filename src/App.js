8d1fa280a8426bd0166ef10599f154c3f895f8cc
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-25 16:01:54 +0300

    added UserList component at App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import UserList from './components/UserList.js/UserList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User List</h1>
+      <UserList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
