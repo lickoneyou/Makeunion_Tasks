308459a31390d9733c0f3da783bc16bf87c7880b
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-25 16:03:01 +0300

    added UserInfoIcons mantine component

---
+++ src/components/User/UserInfoIcons.jsx
@@ -0,0 +1,36 @@
+import { Avatar, Text, Group } from '@mantine/core'
+import { IconPhoneCall, IconAt } from '@tabler/icons-react'
+import classes from './UserInfoIcons.module.css'
+
+export function UserInfoIcons({ avatar, nickName, name, email, phone }) {
+  return (
+    <div className={classes.userWrapper}>
+      <Group wrap="nowrap">
+        <Avatar src={avatar} size={94} radius="md" />
+        <div>
+          <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+            {nickName}
+          </Text>
+
+          <Text fz="lg" fw={500} className={classes.name}>
+            {name}
+          </Text>
+
+          <Group wrap="nowrap" gap={10} mt={3}>
+            <IconAt stroke={1.5} size="1rem" className={classes.icon} />
+            <Text fz="xs" c="dimmed">
+              {email}
+            </Text>
+          </Group>
+
+          <Group wrap="nowrap" gap={10} mt={5}>
+            <IconPhoneCall stroke={1.5} size="1rem" className={classes.icon} />
+            <Text fz="xs" c="dimmed">
+              {phone}
+            </Text>
+          </Group>
+        </div>
+      </Group>
+    </div>
+  )
+}
